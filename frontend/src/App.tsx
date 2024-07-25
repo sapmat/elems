ada77082c3ab3c43469f0e907e12f03c2429d5f3
sapmat <sapprog601@gmail.com>
2024-07-25 15:36:56 +0300

    bottons added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 import "./Styles/App.css";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/episodes" element={<Home />} />
+        <Route path="/characters" element={<Home />} />
+      </Routes>
     </>
   );
 }
--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -1,5 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import "../Styles/Navbar.css";
+
 const Navbar = () => {
-  return <></>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="container">
+      <div className="btn" onClick={() => navigate("/")}>
+        <a>HOME</a>
+      </div>
+      <div className="btn" onClick={() => navigate("/")}>
+        <a>EPISODES</a>
+      </div>
+      <div className="btn" onClick={() => navigate("/")}>
+        <a>CHARACTERS</a>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
