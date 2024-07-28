0fdb01bc2cd2faa4af8b2b3b7888a0f8aa3f19e7
sapmat <sapprog601@gmail.com>
2024-07-28 11:09:55 +0300

    savepoint

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -1,22 +1,5 @@
-import { useNavigate } from "react-router-dom";
-import "../Styles/Navbar.css";
-
 const Navbar = () => {
-  const navigate = useNavigate();
-
-  return (
-    <div className="container">
-      <div className="btn" onClick={() => navigate("/")}>
-        <a>HOME</a>
-      </div>
-      <div className="btn" onClick={() => navigate("/")}>
-        <a>EPISODES</a>
-      </div>
-      <div className="btn" onClick={() => navigate("/")}>
-        <a>CHARACTERS</a>
-      </div>
-    </div>
-  );
+  return <></>;
 };
 
 export default Navbar;
